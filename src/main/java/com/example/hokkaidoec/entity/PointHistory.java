7acb75fa98fba432a403e6662ae43c62f4423a46
hasesyou <hase.syou@example.com>
2026-05-14 17:16:21 +0900

    コンストラクタを焼却しました

--- src/main/java/com/example/hokkaidoec/entity/PointHistory.java
+++ src/main/java/com/example/hokkaidoec/entity/PointHistory.java
@@ -4,71 +4,39 @@ import java.time.LocalDateTime;
 
 public class PointHistory {
 	private int id;
-	private int point_change;
+	private int pointChange;
 	private String reason;
-	private LocalDateTime created_at;
+	private LocalDateTime createdAt;
 
-	public PointHistory(int id, int point_change, String reason, LocalDateTime created_at) {
-		this.id = id;
-		this.point_change = point_change;
-		this.reason = reason;
-		this.created_at = LocalDateTime.now();
-	}
-
-	/**
-	 * @return id
-	 */
 	public int getId() {
 		return id;
 	}
 
-	/**
-	 * @param id セットする id
-	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	/**
-	 * @return point_change
-	 */
-	public int getPoint_change() {
-		return point_change;
+	public int getPointChange() {
+		return pointChange;
 	}
 
-	/**
-	 * @param point_change セットする point_change
-	 */
-	public void setPoint_change(int point_change) {
-		this.point_change = point_change;
+	public void setPointChange(int pointChange) {
+		this.pointChange = pointChange;
 	}
 
-	/**
-	 * @return reason
-	 */
 	public String getReason() {
 		return reason;
 	}
 
-	/**
-	 * @param reason セットする reason
-	 */
 	public void setReason(String reason) {
 		this.reason = reason;
 	}
 
-	/**
-	 * @return created_at
-	 */
-	public LocalDateTime getCreated_at() {
-		return created_at;
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
 	}
 
-	/**
-	 * @param created_at セットする created_at
-	 */
-	public void setCreated_at(LocalDateTime created_at) {
-		this.created_at = created_at;
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
 	}
-
 }
