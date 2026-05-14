bb239b1f3298581285dbc35a9025ea5d8a7caf49
Ninomiya　Noriyuki <norisake404@gmail.com>
2026-05-14 13:15:26 +0900

    pakubakupaku_ECsite初期コミット

---
+++ src/test/java/com/example/demo/PakubakupakuECsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakubakupakuECsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
