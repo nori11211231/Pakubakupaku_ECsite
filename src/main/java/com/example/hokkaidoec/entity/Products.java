8e84faf614db47d0ac5bd82795b4c029f1c54923
ryu-1119 <2223067@stu.hus.ac.jp>
2026-05-14 16:49:04 +0900

    getとsetを追記したよ

--- src/main/java/com/example/hokkaidoec/entity/Products.java
+++ src/main/java/com/example/hokkaidoec/entity/Products.java
@@ -16,11 +16,11 @@ public class Products {
 		this.id = id;
 	}
 
-	public String name() {
+	public String getname() {
 		return name;
 	}
 
-	public void name(String name) {
+	public void setname(String name) {
 		this.name = name;
 	}
 
