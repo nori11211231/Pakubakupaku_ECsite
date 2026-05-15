81ad08b88e36e2f019df9963c9377131a37f06c7
hasesyou <hase.syou@example.com>
2026-05-15 15:40:24 +0900

    ①ポイントエンティティとマッパー作成

--- src/main/java/com/example/hokkaidoec/entity/PointHistory.java
+++ src/main/java/com/example/hokkaidoec/entity/PointHistory.java
@@ -1,5 +1,52 @@
 package com.example.hokkaidoec.entity;
 
+import java.time.LocalDateTime;
+
 public class PointHistory {
+	private int id;
+	private int userId;
+	private int pointChange;
+	private String reason;
+	private LocalDateTime createdAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getPointChange() {
+		return pointChange;
+	}
+
+	public void setPointChange(int pointChange) {
+		this.pointChange = pointChange;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 
 }
--- src/main/java/com/example/hokkaidoec/mapper/PointHistoryMapper.java
+++ src/main/java/com/example/hokkaidoec/mapper/PointHistoryMapper.java
@@ -1,5 +1,12 @@
-package com.example.hokkaidoec.mapper;
-
-public class PointHistoryMapper {
-
-}
+//import java.util.List;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//
+//import com.example.entity.point;
+//
+//@Mapper
+//public interface PointHistoryMapper {
+//
+//	/** IDで商品を1件取得する */
+//	int findPointByUserId(int userId);
+//}
