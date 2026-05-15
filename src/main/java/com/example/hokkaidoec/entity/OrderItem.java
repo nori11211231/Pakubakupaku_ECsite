1c9073b166b8baa41e9c79c99c08a92d5130c067
Taro Tanaka <taro.tanaka@example.com>
2026-05-15 16:14:36 +0900

    OrderItemのgetter/setterの作成

--- src/main/java/com/example/hokkaidoec/entity/OrderItem.java
+++ src/main/java/com/example/hokkaidoec/entity/OrderItem.java
@@ -1,5 +1,25 @@
 package com.example.hokkaidoec.entity;
 
 public class OrderItem {
+	private int quantity;
+	private int productPrice;
 
+	public OrderItem() {
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
 }
