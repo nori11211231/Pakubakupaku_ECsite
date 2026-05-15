fa7231206fda6129031f0a9cb13eb2edf6cf0f45
Taro Tanaka <taro.tanaka@example.com>
2026-05-15 15:36:02 +0900

    UserMapperの作成 名前、メール、住所、電話番号、パスワードを取得

--- src/main/java/com/example/hokkaidoec/mapper/UserMapper.java
+++ src/main/java/com/example/hokkaidoec/mapper/UserMapper.java
@@ -1,5 +1,17 @@
 package com.example.hokkaidoec.mapper;
 
-public class UserMapper {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+@Mapper
+public interface UserMapper {
+
+	/** メールアドレスでユーザを検索する */
+	@Select("SELECT * FROM users WHERE email = #{email}")
+	User findByEmail(String email);
+
+	/** ユーザを登録する */
+	@Insert("INSERT INTO users (name, email,address,phone, password) VALUES (#{name}, #{email},#{address},#{phone} #{password})")
+	void insert(User user);
 }
