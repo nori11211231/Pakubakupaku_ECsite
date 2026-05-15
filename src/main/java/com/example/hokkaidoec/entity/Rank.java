094f4f2bb8e8effce79d81fa9410c5a94e8f3aa4
Taro Tanaka <taro.tanaka@example.com>
2026-05-15 17:10:47 +0900

    Rank.javaの作成

--- src/main/java/com/example/hokkaidoec/entity/Rank.java
+++ src/main/java/com/example/hokkaidoec/entity/Rank.java
@@ -1,5 +1,44 @@
 package com.example.hokkaidoec.entity;
 
 public class Rank {
+	public int rankId;
+	public String rankName;
+	public int minAmount;
+	public double pointRate;
 
+	public Rank() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public int getRankId() {
+		return rankId;
+	}
+
+	public void setRankId(int rankId) {
+		this.rankId = rankId;
+	}
+
+	public String getRankName() {
+		return rankName;
+	}
+
+	public void setRankName(String rankName) {
+		this.rankName = rankName;
+	}
+
+	public int getMinAmount() {
+		return minAmount;
+	}
+
+	public void setMinAmount(int minAmount) {
+		this.minAmount = minAmount;
+	}
+
+	public double getPointRate() {
+		return pointRate;
+	}
+
+	public void setPointRate(double pointRate) {
+		this.pointRate = pointRate;
+	}
 }
