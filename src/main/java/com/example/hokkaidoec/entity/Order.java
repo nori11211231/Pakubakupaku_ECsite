24e9a2d518d627b900538069bdc4e28f21b835b1
hasesyou <hase.syou@example.com>
2026-05-15 16:09:27 +0900

    オーダーのエンティティ作成

--- src/main/java/com/example/hokkaidoec/entity/Order.java
+++ src/main/java/com/example/hokkaidoec/entity/Order.java
@@ -1,5 +1,79 @@
 package com.example.hokkaidoec.entity;
 
+import java.time.LocalDateTime;
+
 public class Order {
+	private int id;
+	private int userId;
+	private String shippingAddress;
+	private LocalDateTime orderDate;
+	private int totalAmount;
+	private int usedPoint;
+	private int earnedPoint;
+	private String orderStatus;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public int getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public int getUsedPoint() {
+		return usedPoint;
+	}
+
+	public void setUsedPoint(int usedPoint) {
+		this.usedPoint = usedPoint;
+	}
+
+	public int getEarnedPoint() {
+		return earnedPoint;
+	}
+
+	public void setEarnedPoint(int earnedPoint) {
+		this.earnedPoint = earnedPoint;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
 
 }
