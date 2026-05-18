7da44928d07f0815fb5079db6cd2c8050fc30f08
Taro Tanaka <taro.tanaka@example.com>
2026-05-18 16:20:39 +0900

    controller,Form,Service,login.html,register.htmlの書き込み

--- src/main/java/com/example/hokkaidoec/controller/UserController.java
+++ src/main/java/com/example/hokkaidoec/controller/UserController.java
@@ -1,43 +1,43 @@
-//package com.example.hokkaidoec.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import com.example.hokkaidoec.form.UserForm;
-//import com.example.service.UserService;
-//
-//@Controller
-//public class UserController {
-//
-//	private final UserService userService;
-//
-//	public UserController(UserService userService) {
-//		this.userService = userService;
-//	}
-//
-//	@GetMapping("/register")
-//	public String showForm(Model model) {
-//		model.addAttribute("form", new UserForm());
-//		return "user/register";
-//	}
-//
-//	@PostMapping("/register")
-//	public String submitForm(
-//			@Validated @ModelAttribute("form") UserForm form,
-//			BindingResult bindingResult,
-//			Model model) {
-//		if (bindingResult.hasErrors()) {
-//			return "user/register"; // エラー時はフォームに戻す
-//		}
-//
-//		userService.register(form);
-//
-//		model.addAttribute("form", form);
-//		return "user/result";
-//	}
-//}
+package com.example.hokkaidoec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.hokkaidoec.form.UserForm;
+import com.example.hokkaidoec.service.UserService;
+
+@Controller
+public class UserController {
+
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		model.addAttribute("form", new UserForm());
+		return "user/register";
+	}
+
+	@PostMapping("/register")
+	public String submitForm(
+			@Validated @ModelAttribute("form") UserForm form,
+			BindingResult bindingResult,
+			Model model) {
+		if (bindingResult.hasErrors()) {
+			return "user/register"; // エラー時はフォームに戻す
+		}
+
+		userService.register(form);
+
+		model.addAttribute("form", form);
+		return "user/result";
+	}
+}
