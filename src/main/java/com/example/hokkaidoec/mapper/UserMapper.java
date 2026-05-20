e429a7e6cb2ea61245562a0f2572444b9d359e70
Taro Tanaka <taro.tanaka@example.com>
2026-05-20 09:40:52 +0900

    理解度が足りていないがUserMapper.java,xmlを訂正

--- src/main/java/com/example/hokkaidoec/mapper/UserMapper.java
+++ src/main/java/com/example/hokkaidoec/mapper/UserMapper.java
@@ -1,21 +1,25 @@
 package com.example.hokkaidoec.mapper;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import com.example.hokkaidoec.entity.User;
 
 @Mapper
 public interface UserMapper {
 
-	/** メールアドレスでユーザを検索する */
-	@Select("SELECT * FROM users WHERE email = #{email}")
-	User findByEmail(String email);
+	User findByUserId(String email);
 
-	/** ユーザを登録する */
-	@Insert("INSERT INTO users (name,email,address,phone,password,point,total_purchase_amount,rank_id) VALUES (#{name}, #{email},#{address},#{phone}, #{password},0,0,#{rankId})")
 	void insert(User user);
+
+	void update(User user);
+
+	//	/** メールアドレスでユーザを検索する */
+	//	@Select("SELECT * FROM users WHERE email = #{email}")
+	//	User findByEmail(String email);
+	//
+	//	/** ユーザを登録する */
+	//	@Insert("INSERT INTO users (name,email,address,phone,password,point,total_purchase_amount,rank_id) VALUES (#{name}, #{email},#{address},#{phone}, #{password},0,0,#{rankId})")
+	//	void insert(User user);
 
 	//	登録済みですをだす。
 }
