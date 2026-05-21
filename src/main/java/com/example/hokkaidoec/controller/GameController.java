903b50b3d79ef0077a7faf61d4666dfbcdfa269e
yamamoto <nokkies1126@gmail.com>
2026-05-21 11:58:10 +0900

    ゲームの確率変更

--- src/main/java/com/example/hokkaidoec/controller/GameController.java
+++ src/main/java/com/example/hokkaidoec/controller/GameController.java
@@ -524,7 +524,7 @@ public class GameController {
 
 		}
 		// 当たり
-		else if (roll <= 480) {
+		else if (roll <= 160) {
 
 			result.setWin(true);
 
@@ -566,7 +566,7 @@ public class GameController {
 					betPoint);
 
 			result.setPointChange(
-					-betPoint * 10);
+					-betPoint);
 
 			result.setVideoPath(
 					"/video/game/hazure.mp4");
