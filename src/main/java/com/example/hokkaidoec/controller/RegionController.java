20386928dea19172175283c2addb8dd80cb316fe
yamamoto <nokkies1126@gmail.com>
2026-05-21 11:46:29 +0900

    地域にポイントを表示するようにする

--- src/main/java/com/example/hokkaidoec/controller/RegionController.java
+++ src/main/java/com/example/hokkaidoec/controller/RegionController.java
@@ -1,13 +1,20 @@
 package com.example.hokkaidoec.controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.hokkaidoec.entity.User;
+
 @Controller
 public class RegionController {
 	@GetMapping("/regions")
-	public String showDetail(Model model) {
+	public String showDetail(Model model, HttpSession session) {
+		// ログインユーザー
+		User loginUser = (User) session.getAttribute("loginUser");
+		model.addAttribute("loginUser", loginUser);
 		return "regions";
 	}
 }
